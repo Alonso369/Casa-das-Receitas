86c1e3cf7c339aa4a20c2cb0c4134cbf38ddc623
Alonso Kraemer <93839204+Alonso369@users.noreply.github.com>
2024-03-20 18:05:26 -0300

    pesquisa

--- JS/index.js
+++ JS/index.js
@@ -1,38 +1,50 @@
 let recipes = null;
 
-fetch('https://forkify-api.herokuapp.com/api/v2/recipes?search=carrot')
-    .then(response => response.json())
-    .then(data => {
-        if (!data) { return }
-        recipes = data.data.recipes;
-        const list = document.getElementById("listaLateral");
-        recipes.forEach(comida => {
-            const cardElement = document.createElement('div');
-            cardElement.classList.add('card');
-            cardElement.addEventListener('click', changeFoodView(comida.id));
-            cardElement.innerHTML = `<div class="img-wrapper">
+const handleSearch = () => {
+    const input = document.getElementById("input-text");
+
+    fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            if (!data) { return }
+            recipes = data.data.recipes;
+
+            const galerySection = document.getElementById("galery-section");
+            if (!!galerySection.previousElementSibling) {
+                galerySection.parentElement.removeChild(galerySection.previousElementSibling)
+            }
+            const list = document.createElement('div');
+            list.classList.add('list');
+            list.innerHTML = ``;
+            recipes.forEach(comida => {
+                const cardElement = document.createElement('div');
+                cardElement.classList.add('card');
+                cardElement.addEventListener('click', () => changeFoodView(comida.id));
+                cardElement.innerHTML = `<div class="img-wrapper">
             <img src=${comida.image_url} alt=${comida.title} title=${comida.title} class="card-img">
             </div>
             <div class="card-info ">
                 <p class="card-main ">${comida.title}</p>
                 <p class="card-complemento ">${comida.publisher}</p>
             </div>`
-            list.appendChild(cardElement);
+                list.appendChild(cardElement);
+            });
+            galerySection.parentElement.insertBefore(list, galerySection)
+        })
+        .catch(error => {
+            console.error('Erro ao obter dados da API:', error);
+            // Lide com o erro de alguma forma, como exibindo uma mensagem de erro na página
         });
-    })
-    .catch(error => {
-        console.error('Erro ao obter dados da API:', error);
-        // Lide com o erro de alguma forma, como exibindo uma mensagem de erro na página
-    });
+}
 
 const changeFoodView = (foodId) => {
-        console.log(foodId)
         fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${foodId}`)
             .then(response => response.json())
             .then(data => {
                     if (!data) { return };
                     const recipe = data.data.recipe;
                     const recipeSection = document.getElementById('cardView');
+                    console.log(recipeSection)
                     recipeSection.innerHTML = `<img class="img-galery" src=${recipe.image_url} alt="Marmitas " title="Marmitas ">
             <div class="mid-info-wrapper">
                 <div class="recipe-mid-info">
@@ -55,7 +67,7 @@ const changeFoodView = (foodId) => {
                                 const recipeItem = document.createElement("div");
                                 recipeItem.classList.add("recipe-item");
                                 recipeItem.innerHTML = `<i class="fa fa-carrot icon-orange"></i>
-                <p>${ingredient.quantity || ``} ${ingredient.unit || ``} ${ingredient.description || ``}</p>`
+                <p class="text-ingredient">${ingredient.quantity || ``} ${ingredient.unit || ``} ${ingredient.description || ``}</p>`
                 bottomWrapper.appendChild(recipeItem);
             })
             console.log(bottomWrapper);
@@ -66,6 +78,8 @@ const changeFoodView = (foodId) => {
         });
 
 };
+
+
 
 // function showSugestao(value) {
 //     const sugestoes = [
@@ -101,5 +115,7 @@ const changeFoodView = (foodId) => {
 // document.addEventListener("click", function(event) {
 //     if (!event.target.closest(".pesquisa")) {
 //         document.getElementById("sugestoes").style.display = "none";
+//     }
+// });     document.getElementById("sugestoes").style.display = "none";
 //     }
 // });
